2294f1d7db238654af7db947edabcae66aaf0b98
Thayna Gimenez <Thayna Gimenez>
2016-10-06 22:31:54 -0300

    primeiro commit

---
+++ src/java/servlets/Publicacao.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author thayn
+ */
+@WebServlet(urlPatterns = {"/Publicacao"})
+public class Publicacao extends HttpServlet {
+
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        PrintWriter writer = response.getWriter();
+        
+        //exibe o jsp no navegador (como se fosse o html)
+        request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
+    }
+
+
+}
